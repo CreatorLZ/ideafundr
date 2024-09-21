f1b1ecd411a1f0879a59c68a42103933a899daa3
Isaac Chimaroke <isaacanyim3@gmail.com>
2024-09-21 10:54:58 +0100

    added testimonials

--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -8,6 +8,12 @@ const CarouselContainer = styled.div`
   padding: 2rem;
   position: relative;
   overflow: hidden;
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
 
   @media (max-width: 768px) {
     padding: 1rem;
